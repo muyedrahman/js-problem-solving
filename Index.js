a45bfc734633fcb2c29f1e8cc3a2259067ce5302
Muyedur rahman <muyedrahman732@gmail.com>
2026-07-10 20:59:22 +0600

    Solve count vowels problem in JavaScript

---
+++ Index.js
@@ -0,0 +1,17 @@
+// 1. Count Vowels
+// Output: 3
+function countVowels(str) {
+  const vowels = "aeiouAEIOU";
+  let count = 0;
+
+  for (const char of str) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+console.log(countVowels("Programming")); 
+
