cf1bd758e56ae6fc489b1be144f3afb76d1d622b
Muyedur rahman <muyedrahman732@gmail.com>
2026-07-18 21:34:00 +0600

    Add Count Even Numbers solution

--- Index.js
+++ Index.js
@@ -94,3 +94,18 @@ function findMin(arr) {
 }
 
 console.log(findMin([5, 12, 8, 25, 10]));
+
+// 9. Count Even Numbers
+function countEven(arr) {
+  let count = 0;
+
+  for (const num of arr) {
+    if (num % 2 === 0) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+console.log(countEven([1, 2, 3, 4, 5, 6]));
