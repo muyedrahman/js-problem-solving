a6a4dad024c1c591e69c3952eb2c290196548400
Muyedur rahman <muyedrahman732@gmail.com>
2026-09-15 21:45:37 +0600

    Add count occurrences solution

--- Index.js
+++ Index.js
@@ -417,3 +417,20 @@ function findUniqueCharacters(str) {
 }
 
 console.log(findUniqueCharacters("programming"));
+
+// 43. Count Occurrences
+
+function countOccurrences(arr, target) {
+  let count = 0;
+
+  for (const num of arr) {
+    if (num === target) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+console.log(countOccurrences([1, 2, 2, 3, 2, 4], 2));
+
